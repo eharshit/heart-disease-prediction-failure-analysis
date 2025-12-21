f87d89d09d5a7d29e4a03a4a717fb4057c56d793
eharshit <harshitgraphy@gmail.com>
2025-12-21 20:58:50 +0530

    Streamlit configure

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 import shap
 from streamlit_shap import st_shap
 import warnings
-
 warnings.filterwarnings('ignore')
+
+# ---- Page Configuration ----
+st.set_page_config(
+    page_title = "Heart Disease Predictor",
+    layout= "centered",
+    initial_sidebar_state="expanded"
+)
 
